a5b15c55ab792111c050ebe35b5acb51f611f89a
Muhammad Iqbal Tawakal <mit.iqi@gmail.com>
2014-11-08 01:30:24 +0100

    Added basic changed stage interactions

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,44 @@
 // This is the main javascript code
+
+// Hide other block in the beginning
+$('#singNum').hide();
+$('#singerName').hide();
+$('#klasemen').hide();
+$('#babak1').hide();
+$('#babak2').hide();
+$('#babak3').hide();
+$('#win').hide();
+
+// Add event for all button
+$('#btnCreate').click(function() {
+  // hide the crete competition block
+  $('#compName').hide();
+  // show the next block
+  $('#singNum').show();
+});
+
+$('#btnSingerNum').click(function() {
+  $('#singNum').hide();
+  $('#singerName').show();
+});
+
+$('#btnSingerName').click(function() {
+  $('#singerName').hide();
+  $('#klasemen').show();
+  $('#babak1').show();
+});
+
+$('#btnBabak1').click(function() {
+  $('#babak1').hide();
+  $('#babak2').show();
+});
+
+$('#btnBabak2').click(function() {
+  $('#babak2').hide();
+  $('#babak3').show();
+});
+
+$('#btnBabak3').click(function() {
+  $('#babak3').hide();
+  $('#win').show();
+});
